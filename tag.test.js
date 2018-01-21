c1b618a9d46c8b8b9a773d71e4e258be918f4056
parth <srivastavaparth2016@gmail.com>
2018-01-22 01:51:57 +0530

    tag.js test case added

--- tag.test.js
+++ tag.test.js
@@ -9,3 +9,8 @@ test('Should work for <b>Daniel says</b>: "2 < 3"', () => {
 test('Should work for no arguments', () => {
   expect(html`<b> says</b>: ""`).toBe(undefined);
 });
+
+const usernam = 'D<a<n<i<el';
+test('Should work for <b>D<a<n<i<el says</b>: "2 < 3"', () => {
+  expect(html`<b>${usernam} says</b>: "${comment}"`).toBe('<b>D&lt;a&lt;n&lt;i&lt;el says</b>: "2 &lt; 3"');
+});
