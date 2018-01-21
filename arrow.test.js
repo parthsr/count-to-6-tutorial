5a4e7fb9c6c136b2661b12a5702d43e6dadc63f9
parth <srivastavaparth2016@gmail.com>
2018-01-21 18:32:52 +0530

    adding third test case for arrow.js

--- arrow.test.js
+++ arrow.test.js
@@ -7,11 +7,11 @@ test('this is the first test to check its use case', () => {
 test('this is the 2nd test to check with no arguments', () => {
   expect().toBe(undefined);
 });
-//
-// test('this is the third test to check with numbers in strings', () => {
-//   expect(arrow('123', 'parth')).toBe('[123,parth] becomes "1p"');
-// });
-//
+
+test('this is the third test to check with numbers in strings', () => {
+  expect(arrow('123', 'parth')).toBe('[123,parth] becomes "1p"');
+});
+
 // test('this is the fourth test to check with empty strings as arguments', () => {
 //   expect(arrow('', '')).toBe('[,] becomes "undefinedundefined"');
 // });
