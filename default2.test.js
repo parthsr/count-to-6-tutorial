1887be60481de1082a76ffe59c0d28be2a5ba6e3
parth <srivastavaparth2016@gmail.com>
2018-01-21 22:53:49 +0530

    test cases for default2.js

--- default2.test.js
+++ default2.test.js
@@ -1,5 +1,14 @@
 const makeImportant = require('./default2');
 
 test('test 1 for use case', () => {
-  expect(makeImportant('hello', 5)).toBe('hello!!!!!');
+  expect(makeImportant('hello', 6)).toBe('hello!!!!!!');
+});
+
+test('test 2 for 1 argument', () => {
+  expect(makeImportant('hello')).toBe('hello!!!!!');
+});
+
+
+test('test 3 for empty string argument', () => {
+  expect(makeImportant('')).toBe('');
 });
