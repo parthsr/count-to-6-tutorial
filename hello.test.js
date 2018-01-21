eb32d93e5d8c086b6d4438b0c5a19952a4736726
parth <srivastavaparth2016@gmail.com>
2018-01-21 17:35:50 +0530

    use case test case

---
+++ hello.test.js
@@ -0,0 +1,13 @@
+let print = require('./hello.js');
+
+test('testing the hello print', function(){
+  expect(print()).toBe('HELLO ES6');
+});
+
+// test('testing the hello print with different case', function(){
+//   expect(print()).not.toBe('Hello es6');
+// });
+//
+// test('testing the hello print with different string', function(){
+//   expect(print()).not.toBe('hahaha');
+// });
