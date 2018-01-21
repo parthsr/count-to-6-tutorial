0adea19ec4eb9baedbc78e51c303aa425989f7a5
parth <srivastavaparth2016@gmail.com>
2018-01-21 18:32:34 +0530

    adding second test case for arrow.js

--- arrow.test.js
+++ arrow.test.js
@@ -4,9 +4,9 @@ test('this is the first test to check its use case', () => {
   expect(arrow('parth', 'is', 'cool')).toBe('[parth,is,cool] becomes "pic"');
 });
 
-// test('this is the 2nd test to check with no arguments', () => {
-//   expect().toBe(undefined);
-// });
+test('this is the 2nd test to check with no arguments', () => {
+  expect().toBe(undefined);
+});
 //
 // test('this is the third test to check with numbers in strings', () => {
 //   expect(arrow('123', 'parth')).toBe('[123,parth] becomes "1p"');
