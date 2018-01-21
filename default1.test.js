7f0cc2d2505df709615fdf4b7604163380e3a305
parth <srivastavaparth2016@gmail.com>
2018-01-21 22:22:24 +0530

    adding test cases for defined1.js

--- default1.test.js
+++ default1.test.js
@@ -3,3 +3,15 @@ const defaults = require('./default1');
 test('This is test 1 for the use case', () => {
   expect(defaults(1, 2)).toBe(1.5);
 });
+
+test('This is test 2 for the use case', () => {
+  expect(defaults(1)).toBe(1);
+});
+
+test('This is test 3 for the use case', () => {
+  expect(defaults(undefined, 1)).toBe(0.5);
+});
+
+test('This is test 4 for the use case', () => {
+  expect(defaults(undefined, undefined)).toBe(0.5);
+});
