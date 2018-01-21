b6c739db851848eec9626244634961a08387b22a
parth <srivastavaparth2016@gmail.com>
2018-01-21 22:26:22 +0530

    test cases added for the default1.js

--- default1.test.js
+++ default1.test.js
@@ -16,10 +16,10 @@ test('This is test 4 for the use case', () => {
   expect(defaults(undefined, undefined)).toBe(0.5);
 });
 
-test('This is test 5 by addig 3 parameters', () => {
+test('This is test 5 by adding 3 parameters', () => {
   expect(defaults(5, 4, 3)).toBe(4.5);
 });
 
-test('This is test 5 by addig 3 parameters', () => {
+test('This is test 5 by adding no parameters', () => {
   expect(defaults()).toBe(0.5);
 });
