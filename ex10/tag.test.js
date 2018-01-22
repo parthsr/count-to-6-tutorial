53928591998ab30bc5bd1d68331d1b00d6215274
parth <srivastavaparth2016@gmail.com>
2018-01-22 19:26:49 +0530

    adding folders

--- ex10/tag.test.js
+++ ex10/tag.test.js
@@ -6,11 +6,11 @@ test('Should work for <b>Daniel says</b>: "2 < 3"', () => {
   expect(html`<b>${username} says</b>: "${comment}"`).toBe('<b>Daniel says</b>: "2 &lt; 3"');
 });
 
-test('Should work for no arguments', () => {
+test('would not work for no arguments', () => {
   expect(html`<b> says</b>: ""`).toBe(undefined);
 });
 
-const usernam = 'D<a<n<i<el';
+usernam = 'D<a<n>i<el';
 test('Should work for <b>D<a<n<i<el says</b>: "2 < 3"', () => {
-  expect(html`<b>${usernam} says</b>: "${comment}"`).toBe('<b>D&lt;a&lt;n&lt;i&lt;el says</b>: "2 &lt; 3"');
+  expect(html`<b>${usernam} says</b>: "${comment}"`).toBe('<b>D&lt;a&lt;n&gt;i&lt;el says</b>: "2 &lt; 3"');
 });
